dd6bb406251a222b3d43a09012ef40d0b228e6ce
Assunta Oropallo <assunta.oropallo@gmail.com>
2017-06-07 10:44:13 +0200

    modifica stampa file di log, ora stampa un file per ogni log

--- logsTravis.py
+++ logsTravis.py
@@ -13,7 +13,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-maxnumberbuilds=10
+maxnumberbuilds=2
 
 
 ansi_escape = re.compile(r'\x1b\[[0-9]+(K)?(;[0-9])?(m)?')
@@ -25,14 +25,12 @@ if len(sys.argv)>1:
     builds = t.builds(slug=repo.slug)
     for count in range(0,min(maxnumberbuilds,len(builds))):
         build=builds[count]
-        job_ids=build.job_ids
-        for job in t.jobs(ids=job_ids):
+        for job_id in build.job_ids:
+            job=t.job(job_id)
             fOut = open('logs/' + sys.argv[1].split('/')[-1] + '-' + build.number + '-'+str(job.log_id)+'.txt', 'w+')
-
             text=t.log(job.log_id).body
             text=ansi_escape.sub('', text)
             print fOut.name
-            #print (text)
             fOut.write(text)
 else:
     print ("Inserire il parametro a riga di comando con il nome del repository di cui vuoi avere la build")
